afde5086af16c98beec0132d918292db8855c97b
pranav280601 <72141278+pranav280601@users.noreply.github.com>
2021-05-15 18:06:57 +0530

    Update PlayPredictor_CaseStudy.py

--- PlayPredictor_CaseStudy.py
+++ PlayPredictor_CaseStudy.py
@@ -2,10 +2,6 @@ from sklearn.datasets import load_iris
 
 from sklearn.neighbors import KNeighborsClassifier
 
-from sklearn.datasets import load_iris
-
-from sklearn.neighbors import KNeighborsClassifier
-
 from sklearn.model_selection import train_test_split
 
 from scipy.spatial import distance
